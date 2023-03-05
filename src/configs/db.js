b0cc2fdcf17b3f5992802039d4e7202ff8c0c117
AvdheshPal <Avdheshpal2201@gmail.com>
2023-03-06 05:26:27 +0530

    Initial commit

---
+++ src/configs/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    return mongoose.connect("mongodb+srv://sachida44:sachida44@cluster0.xiqh3.mongodb.net/udemy");
+    
+
+    // This is for me for testing purpose... Please don't remove it.... BIGDADDY WALA THANKYOU
+    //return mongoose.connect("mongodb://127.0.0.1:27017/udemy");
+};
+
+
+// mongodb+srv://sachida44:sachida44@cluster0.xiqh3.mongodb.net/test
